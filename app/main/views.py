027ea5002a24d278134ab327917d2d0e58581e1c
Kiptim <kiptim54@gmail.com>
2018-04-17 17:17:33 +0300

    adding

--- app/main/views.py
+++ app/main/views.py
@@ -5,8 +5,16 @@ from flask import render_template
 @main.route('/')
 def index():
     '''
-    function renders the landing page
+    function renders the landing page with news sources
     '''
     title = "NewsHighlighs | Home"
 
     return render_template('index.html', title=title)
+
+@main.route('/articles')
+def articles():
+    '''
+    function shows the news articles
+    '''
+    title= "NewsHighlighs | Articles"
+    return render_template('articles.html', title =title)

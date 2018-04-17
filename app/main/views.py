ab706bd9ad9076359a8158a4ff1b428b098f207e
Kiptim <kiptim54@gmail.com>
2018-04-17 16:39:02 +0300

    add navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,12 @@
-
+from flask import render_template,request,redirect,url_for
+from . import main
+from flask import render_template
     
+@main.route('/')
+def index():
+    '''
+    function renders the landing page
+    '''
+    title = "NewsHighlighs | Home"
+
+    return render_template('index.html', title=title)
